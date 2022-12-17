2e595e802d660eb3a123b9cd52f7fc15980de0b4
Bashir Tarif <112946825+bash199@users.noreply.github.com>
2022-12-17 18:22:41 +0200

    files modified3

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Card } from 'flowbite-react'
-import { Api } from '../../api/Api'
+import axios from 'axios'
 import ListItemComp from '../ListItemComp'
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
 
   useEffect(()=>{
     const getUsers = async () =>{
-      const {data} = await Api.get("/users")
+      const {data} = await axios.get("https://bashobankapp.onrender.com/api/users")
       setUsers(data)
     }
     getUsers()
